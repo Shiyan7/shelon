0f2a04d7916177d4ffb2a0702f343109a4fa6738
Jeka Shiyan <ncringe31@gmail.com>
2022-08-11 00:18:31 +0300

    some fixes

--- src/js/functions/disable-scroll.js
+++ src/js/functions/disable-scroll.js
@@ -1,19 +1,3 @@
 export const disableScroll = () => {
-
-  setTimeout(function () {
-
-    if (!document.body.hasAttribute("data-body-scroll-fix")) {
-
-      let scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
-
-      document.body.setAttribute("data-body-scroll-fix", scrollPosition);
-      document.body.style.overflow = "hidden";
-      document.body.style.position = "fixed";
-      document.body.style.top = "-" + scrollPosition + "px";
-      document.body.style.left = "0";
-      document.body.style.width = "100%";
-
-    }
-
-  }, 15);
+  document.body.classList.add('lock')
 }
--- src/js/functions/enable-scroll.js
+++ src/js/functions/enable-scroll.js
@@ -1,16 +1,3 @@
 export const enableScroll = () => {
-  if (document.body.hasAttribute("data-body-scroll-fix")) {
-
-    let scrollPosition = document.body.getAttribute("data-body-scroll-fix");
-
-    document.body.removeAttribute("data-body-scroll-fix");
-
-    document.body.style.overflow = "";
-    document.body.style.position = "";
-    document.body.style.top = "";
-    document.body.style.left = "";
-    document.body.style.width = "";
-
-    window.scroll(0, scrollPosition);
-  }
+  document.body.classList.remove('lock')
 }
