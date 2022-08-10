5c318bec5109d4b4ddbdb1c395b183240b6f4f7a
Jeka Shiyan <ncringe31@gmail.com>
2022-08-10 14:37:50 +0300

    some fixes

--- src/js/functions/header-height.js
+++ src/js/functions/header-height.js
@@ -2,3 +2,7 @@ export const getHeaderHeight = () => {
   const headerHeight = document?.querySelector('.header').offsetHeight;
   document.querySelector(':root').style.setProperty('--header-height', `${headerHeight}px`);
 }
+
+getHeaderHeight();
+
+window.addEventListener('resize', getHeaderHeight);
