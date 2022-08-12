f13ed76099fb6ed353fa680503649f9030b21ba1
Jeka Shiyan <ncringe31@gmail.com>
2022-08-12 09:10:13 +0300

    some fixes

--- src/js/functions/header-height.js
+++ src/js/functions/header-height.js
@@ -1,8 +1,6 @@
 export const getHeaderHeight = () => {
   const headerHeight = document?.querySelector('.header').offsetHeight;
-  const menuHeight = document?.querySelector('.menu').offsetHeight;
   document.querySelector(':root').style.setProperty('--header-height', `${headerHeight}px`);
-  document.querySelector(':root').style.setProperty('--menu-height', `${menuHeight}px`);
 }
 
 getHeaderHeight();
