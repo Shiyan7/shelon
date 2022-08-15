5b6b5246274074d142b77eb76cb26cc0f1e778bc
Jeka Shiyan <ncringe31@gmail.com>
2022-08-15 16:07:49 +0300

    fix animation

--- src/js/components/members-slider.js
+++ src/js/components/members-slider.js
@@ -1,29 +1,21 @@
-import Swiper, { Thumbs, Autoplay  } from 'swiper';
-
 document.addEventListener('DOMContentLoaded', () => {
-  const membersSliderNav = new Swiper('.members-slider__nav', {
-    modules: [Autoplay],
-    autoplay: {
-      delay: 3000
-    },
-    slidesPerView: 'auto',
-    direction: 'vertical',
-  })
+  const membersItems = document.querySelectorAll('.members-item__title');
+  const membersSlides = document.querySelectorAll('.members-slide')
 
-  new Swiper('.members-slider__main', {
-    modules: [Thumbs, Autoplay],
-    spaceBetween: 15,
-    slidesPerView: 1,
-    autoplay: {
-      delay: 3000
-    },
-    breakpoints: {
-      577: {
-        spaceBetween: 20
-      }
-    },
-    thumbs: {
-      swiper: membersSliderNav,
-    }
+  membersItems.forEach(el => {
+    const parent = el.closest('.members-item')
+    const id = parent?.dataset.slidePath;
+
+    el.addEventListener('click', () => {
+
+      const target = document.querySelector(`.members-slide[data-slide-target='${id}']`)
+
+      membersSlides?.forEach(slide => {
+        membersItems.forEach(el => el.closest('.members-item').classList.remove('active'))
+        slide?.classList.remove('active')
+        target?.classList.add('active')
+        parent?.classList.add('active')
+      })
+    })
   })
 })
