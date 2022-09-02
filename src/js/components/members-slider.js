a93dcd93889b474a03e844af77d3f1ca446dc963
Jeka Shiyan <ncringe31@gmail.com>
2022-09-02 18:35:42 +0300

    some fixes

--- src/js/components/members-slider.js
+++ src/js/components/members-slider.js
@@ -6,26 +6,24 @@ ScrollMagicPluginGsap(ScrollMagic, TweenMax, TimelineMax);
 
 document.addEventListener('DOMContentLoaded', () => {
   const controller = new ScrollMagic.Controller();
-
   const container = document.querySelector(".members");
 
   const animation = new TimelineMax()
-  .staggerFromTo(".js-section", 0, {
-      autoAlpha: 0
-    },
-    {
-      autoAlpha: 1
+  animation.staggerFromTo(".js-section", 0.1, {
+      opacity: 0,
+      visibility: 'hidden'
+    }, {
+      opacity: 1,
+      visibility: 'visible'
     }, 1);
 
   new ScrollMagic.Scene({
     triggerElement: container,
     triggerHook: 'onLeave',
-    duration: "200%"
+    duration: "300%"
   })
-  .setClassToggle('.members', 'members--active')
   .setPin(container)
   .setTween(animation)
   .addIndicators()
   .addTo(controller);
-
 })
