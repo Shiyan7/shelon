c7faf1c9941cdef49da703765d46a916aaf6f8e9
Jeka Shiyan <ncringe31@gmail.com>
2022-09-08 21:16:21 +0300

    some fixes

--- src/js/components/fullpage.js
+++ src/js/components/fullpage.js
@@ -5,13 +5,17 @@ document.addEventListener('DOMContentLoaded', () => {
   if(window.outerWidth >= 1024) {
     const header = document.querySelector('.header');
 
-    new fullpage('#fullpage', {
+    const fp = new fullpage('#fullpage', {
       scrollingSpeed: 600,
       normalScrollElements: '.modal',
       scrollHorizontally: true,
       verticalCentered: false,
+      scrollBar: true,
+      controlArrows: false,
+      credits: { enabled: false},
 
       onLeave: function(section, slide, position, direction){
+        fp.setScrollingSpeed(600);
         if(!slide.isFirst) {
           header?.classList.add('header--fixed');
           header?.classList.add('header--light');
@@ -19,6 +23,9 @@ document.addEventListener('DOMContentLoaded', () => {
           header?.classList.remove('header--fixed');
           header?.classList.remove('header--light');
         }
+      },
+      onSlideLeave: function() {
+        fp.setScrollingSpeed(400);
       },
       afterSlideLoad: function() {
         const item = document.querySelector('.members-item.active');
@@ -28,7 +35,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const target = document.querySelector(`.members-slide[data-slide-target='${id}']`)
 
         target.classList.add('active')
-      }
+      },
     });
   }
 });
--- src/js/components/members-slider.js
+++ src/js/components/members-slider.js
@@ -1,23 +1,23 @@
 document.addEventListener('DOMContentLoaded', () => {
-  if(window.outerWidth <= 1024) {
-    const membersItems = document.querySelectorAll('.members-item__title');
-    const membersSlides = document.querySelectorAll('.members-slide')
+  const membersItems = document.querySelectorAll('.members-item__title');
+  const membersSlides = document.querySelectorAll('.members-slide')
 
-    membersItems?.forEach(el => {
-      const parent = el.closest('.members-item')
-      const id = parent?.dataset.slidePath;
+  membersItems?.forEach(el => {
+    const parent = el.closest('.members-item')
+    const id = parent?.dataset.slidePath;
 
-      el.addEventListener('click', () => {
+    el.addEventListener('click', () => {
 
-        const target = document.querySelector(`.members-slide[data-slide-target='${id}']`)
+      const target = document.querySelector(`.members-slide[data-slide-target='${id}']`)
 
-        membersSlides?.forEach(slide => {
-          membersItems.forEach(el => el.closest('.members-item').classList.remove('active'))
+      membersSlides?.forEach(slide => {
+        membersItems.forEach(el => el.closest('.members-item').classList.remove('active'))
+        setTimeout(() => {
           slide?.classList.remove('active')
           target?.classList.add('active')
-          parent?.classList.add('active')
-        })
+        }, 400)
+        parent?.classList.add('active')
       })
     })
-  }
+  })
 })
