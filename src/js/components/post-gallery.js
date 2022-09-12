cc2c2f443848194ee1e934abe93e6a490e8217ec
Jeka Shiyan <ncringe31@gmail.com>
2022-09-12 13:19:57 +0300

    post slider

--- src/js/components/post-gallery.js
+++ src/js/components/post-gallery.js
@@ -1,7 +1,17 @@
-import Swiper from 'swiper'
+import Swiper, {Pagination, Navigation} from 'swiper'
 
 document.addEventListener('DOMContentLoaded', () => {
   new Swiper('.post-gallery', {
+    modules: [Navigation, Pagination],
     spaceBetween: 20,
+    pagination: {
+      el: '.post-gallery__pag',
+      type: 'bullets',
+      clickable: true
+    },
+    navigation: {
+      prevEl: '.post-gallery__btn--prev',
+      nextEl: '.post-gallery__btn--next',
+    }
   })
 });
